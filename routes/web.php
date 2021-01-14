115bba405e00a345b4e615a2b0a1c805a9f3ee04
ProgNAVJ <nicolas.a.vegajara@gmail.com>
2021-01-14 20:58:22 -0300

    cambios

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,3 @@ Route::get('cursos',[CursoController::class, 'index']);
 Route::get('cursos/create',[CursoController::class, 'create']);
 
 Route::get('cursos/{curso}', [CursoController::class, 'show']);
-
-/*
-Route::get('cursos/{curso}/{categoria?}', function ($curso, $categoria = null) {
-    if($categoria){
-        return "En esta página podras crear un curso: $curso, de la categoría, $categoria";
-    }
-    else
-        return "Bienvenido al curso: $curso";
-});
